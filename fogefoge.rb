e8e8cd4bd049f7eca9cb02defe52c65ad9b097c4
vinicius.padilha <vinicius.padilha@euax.com.br>
2022-04-28 15:46:25 -0300

    Criação de mapas

---
+++ fogefoge.rb
@@ -0,0 +1,10 @@
+require_relative 'ui'
+
+def joga(nome)
+
+end
+
+def inicia_fogefoge
+    nome = boas_vindas
+    joga nome
+end
---
+++ ui.rb
@@ -0,0 +1,8 @@
+def boas_vindas
+    puts "Bem vindo ao Foge-foge"
+    puts "Qual é o seu nome?"
+    nome = gets.strip
+    puts "\n\n\n\n\n"
+    puts "Começaremos o jogo agora, #{nome}"
+    nome
+end
