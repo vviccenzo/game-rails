a0f327b70eda8b7a35c09bcbfa4b439e117359de
vinicius.padilha <vinicius.padilha@euax.com.br>
2022-04-29 10:12:05 -0300

    Refaturando

--- fogefoge.rb
+++ fogefoge.rb
@@ -1,10 +1,13 @@
 require_relative 'ui'
 
 def encontrar_jogador(mapa)
+    caractere_do_heroi = "H"
     for linha = 0..(mapa.size-1)
-        if mapa[linha].include? "H"
-            for coluna = 0..(mapa[linha].size-1)
-                if mapa[linha][coluna] == "H"
+        linha_atual = mapa[linha]
+        if heroi_esta_nessa_linha = linha_atual.include? caractere_do_heroi
+            for coluna = 0..(linha.size-1)
+                heroi_esta_aqui = linha_atual[coluna] == caractere_do_heroi
+                if heroi_esta_aqui
                     return [linha, coluna]
                 end
             end
