f9c556767f3c48955589f4ff5200ee09d3568fac
vinicius.padilha <vinicius.padilha@euax.com.br>
2022-04-29 09:29:29 -0300

    Leitura de movimentos

--- fogefoge.rb
+++ fogefoge.rb
@@ -1,5 +1,17 @@
 require_relative 'ui'
 
+def encontrar_jogador(mapa)
+    for linha = 0..(mapa.size-1)
+        if mapa[linha].include? "H"
+            for coluna = 0..(mapa[linha].size-1)
+                if mapa[linha][coluna] == "H"
+                    return [linha, coluna]
+                end
+            end
+        end
+    end
+end
+
 def le_mapa(numero)
     arquivo = "map#{numero}.txt"
     texto = File.read arquivo
